5c7c1cac34acea65dd7eaa2e5bd683e76ffc1229
Andrew Kalil <kalilmagal@gmail.com>
2021-10-18 09:54:42 -0300

    initial setup

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <section>Home</section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
---
+++ src/vue-apollo.js
@@ -0,0 +1,53 @@
+import Vue from 'vue';
+import VueApollo from 'vue-apollo';
+import { createApolloClient } from 'vue-cli-plugin-apollo/graphql-client';
+
+// Install the vue plugin
+Vue.use(VueApollo);
+
+// Name of the localStorage item
+const AUTH_TOKEN = 'apollo-token';
+
+// Http endpoint
+const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP;
+
+// Config
+const defaultOptions = {
+  httpEndpoint,
+  // LocalStorage token
+  tokenName: AUTH_TOKEN,
+  persisting: false,
+  ssr: false,
+  getAuth: () => {
+    const token = process.env.VUE_APP_GITHUB_GRAPHQL_AUTH_TOKEN;
+    if (!token) return '';
+    return `Bearer ${token}`;
+  },
+};
+
+export function createProvider(options = {}) {
+  const { apolloClient, wsClient } = createApolloClient({
+    ...defaultOptions,
+    ...options,
+  });
+  apolloClient.wsClient = wsClient;
+
+  const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+    defaultOptions: {
+      $query: {
+        // fetchPolicy: 'cache-and-network',
+      },
+    },
+    errorHandler(error) {
+      // eslint-disable-next-line no-console
+      console.log(
+        '%cError',
+        'background: red; color: white; padding: 2px 4px; border-radius: 3px; font-weight: bold;',
+        error.message
+      );
+    },
+  });
+
+  return apolloProvider;
+}
